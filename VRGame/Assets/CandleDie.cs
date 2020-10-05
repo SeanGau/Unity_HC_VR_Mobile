9feba023f423bb623d515deadc0d74f33c848fad
SeanGau <rrtw0627@gmail.com>
2020-10-05 21:26:56 +0800

    add event

---
+++ VRGame/Assets/CandleDie.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleDie : MonoBehaviour
+{
+    private ParticleSystem[] flames;
+    private void Awake()
+    {
+        flames = this.GetComponentsInChildren<ParticleSystem>();
+    }
+    public void dieCandles()
+    {
+        foreach (var item in flames)
+        {
+            var emission = item.emission;
+            emission.enabled = false;
+        }
+    }
+}
